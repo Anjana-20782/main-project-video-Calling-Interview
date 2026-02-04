35013935719cbc0f4e84186d5162b92e02cafdfc
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-02-04 12:48:35 +0530

    new added server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -49,7 +49,7 @@ if (ENV.NODE_ENV === 'production') {
   });
 }
 
-
+//server started
 connectDB().then(() => {
   app.listen(ENV.PORT, () => {
     console.log("server is running on port :", ENV.PORT);
