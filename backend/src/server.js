c55e825a0516c8dd822a1a442be9c8ccacf43fd8
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-02-04 12:34:34 +0530

    new contents added in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,60 +1,57 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import path from 'path';
 import cors from "cors";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./lib/inngest.js"; 
 
-import { ENV } from "./lib/env.js";
-import { connectDB } from "./lib/db.js";
+import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
+import { inngest, functions } from "./lib/inngest.js";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
+console.log(__dirname)
 
-const app = express()
 
-const __dirname = path.resolve()
+// middleware
+app.use(express.json());
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  process.env.PROD_URL
+];
 
-//middleware
-app.use(express.json())
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
-//credentials:true meaning?? => server allows a browser to include cookies on request
-app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 
+// API routes
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-
-app.get("/health",(req,res) => {
-
-    res.status(200).json({msg:"api is up and running"})
+app.get('/api', (req, res) => {
+  res.json({ message: 'API working' });
 });
 
-app.get("/books",(req,res) => {
+if (ENV.NODE_ENV === 'production') {
+  app.use(
+    express.static(path.join(__dirname, '../../frontend/dist'))
+  );
 
-    res.status(200).json({msg:"this is the books endpoint"})
-});
-
-// make our app ready for deployment
-if(ENV.NODE_ENV === "production")
-{
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-
-    app.get("/{*any}", (req,res) => {
-
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.join(__dirname, '../../frontend/dist/index.html')
+    );
+  });
 }
 
-//starting server
 
-const startServer = async () => {
-
-    try {
-        await connectDB();
-       app.listen(ENV.PORT , () =>
-     console.log("Server ruuning on port:", ENV.PORT));     
-    } catch (error) {
-        console.error("Error starting the server");
-        
-    }
-};
-
-startServer();
+connectDB().then(() => {
+  app.listen(ENV.PORT, () => {
+    console.log("server is running on port :", ENV.PORT);
+  });
+});
